29101dcc6402501090dc143dda1ee796a258f16d
busracetinelli <busracetinell>
2025-02-03 12:46:57 +0300

    ttest

--- MazeChallengeBot/Program.cs
+++ MazeChallengeBot/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddScoped<IOpenAPIService, OpenAPIManager>();
 builder.Services.AddScoped<HttpClient>();
 var app = builder.Build();
 
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
